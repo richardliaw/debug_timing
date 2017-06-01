f3ce7861578e6fc6f2f2fb83b5f3df3545a38fdb
Richard <rliaw@berkeley.edu>
2017-05-31 17:41:49 -0700

    spacing

--- ps.py
+++ ps.py
@@ -2,16 +2,16 @@ import numpy as np
 from misc import add_delta
 
 class ParameterServer():
-	def __init__(self, env):
-  		self._policy = LSTMPolicy(env.observation_space.shape, env.action_space.n, 0)
-		self.weights = self._policy.get_weights()
-		# may need to change this into a model
+  def __init__(self, env):
+      self._policy = LSTMPolicy(env.observation_space.shape, env.action_space.n, 0)
+    self.weights = self._policy.get_weights()
+    # may need to change this into a model
 
-	def add_delta(self, delta):
-		self.weights = add_delta(self.weights, delta)
+  def add_delta(self, delta):
+    self.weights = add_delta(self.weights, delta)
 
-	def async_add(self, delta):
-		pass
+  def async_add(self, delta):
+    pass
 
-	def get_weights():
-		return self.weights
+  def get_weights():
+    return self.weights
