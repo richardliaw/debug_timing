94f75889617973712ab9f9ac45751bc975a0f03b
Richard <rliaw@berkeley.edu>
2017-05-31 17:43:54 -0700

    indent

--- ps.py
+++ ps.py
@@ -3,7 +3,7 @@ from misc import add_delta
 
 class ParameterServer():
   def __init__(self, env):
-      self._policy = LSTMPolicy(env.observation_space.shape, env.action_space.n, 0)
+    self._policy = LSTMPolicy(env.observation_space.shape, env.action_space.n, 0)
     self.weights = self._policy.get_weights()
     # may need to change this into a model
 
